fdc1c4f1712c322b5c44241e2860ce9f6eb0ca3c
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-07-26 13:05:09 -0700

    updating ./learning-terraform

---
+++ 02-variables/main.tf
@@ -0,0 +1,20 @@
+variable "a" {
+  default = 100
+}
+
+// variable block is having one reference, here a is refernce
+
+output = "a" {
+        value = var.a
+}
+
+// variable in terraform can be accessed by var.var_name or ${var.var_name}
+
+
+output "x" {
+        value= "Value of a = ${var.a}"
+}
+
+
+// If you access variables with any combination strings then ${} is preffered
+
