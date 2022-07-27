983221c960f3cad6396bd047a625afefac3a0fb5
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-07-27 14:24:51 -0700

    updating ./learning-terraform

---
+++ 04-loops/approach02/module/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "web" {
+  count         = var.no-of-instances
+  ami           = "ami-0ed52d9176c96bc18"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform"
+  }
+}
