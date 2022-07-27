0d3685567d1fc6f8ef73931450fa4a94db0c805a
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-07-27 14:02:03 -0700

    updating ./learning-terraform

---
+++ 04-loops/approach01/main.tf
@@ -0,0 +1,13 @@
+variable "no-of-instances" {
+  default = 2
+}
+
+resource "aws_instance" "web" {
+  count         = var.no-of-instances
+  ami           = "ami-0ed52d9176c96bc18"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform"
+  }
+}
