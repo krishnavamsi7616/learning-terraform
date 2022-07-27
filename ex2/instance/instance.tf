746ff4f14b05bf90f338745d75b208e50a3e09ab
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-07-27 00:23:29 -0700

    updating ./learning-terraform

--- ex2/instance/instance.tf
+++ ex2/instance/instance.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "web" {
   ami           = "ami-0ed52d9176c96bc18"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [var.SGID]
 
   tags = {
     Name = "terraform"
   }
 }
+variable "SGID" {}
