e3f9738e351ef69f223837933e2ffdc38674c1e0
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-07-28 13:36:23 -0700

    updating ./learning-terraform

--- 06-conditions/main.tf
+++ 06-conditions/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   // count      = 0 (We can make some condition to come with a value based on that this resource can be created)
-  ami           = "ami-0ed52d9176c96bc18"
+  ami           = data.aws_ami.example.image_id
   instance_type = var.instance_type == null ? "t3.micro" : var.instance_type
 
   tags = {
@@ -12,3 +12,13 @@ variable "instance_type" {
   default = null
 }
 
+locals {
+  instance_type =  var.instance_type == null ? "t3.micro" : var.instance_type
+}
+
+data "aws_ami" "example" {
+  executable_users = ["self"]
+  most_recent      = true
+  name_regex       = "base-image-with-ansible-installed"
+  owners           = ["self"]
+}
