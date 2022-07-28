e12a36501dc5af400fee211783ba3b4543420d9d
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-07-28 13:38:52 -0700

    updating ./learning-terraform

--- 06-conditions/main.tf
+++ 06-conditions/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   // count      = 0 (We can make some condition to come with a value based on that this resource can be created)
   ami           = data.aws_ami.example.image_id
-  instance_type = var.instance_type == null ? "t3.micro" : var.instance_type
+  instance_type = local.instance_type
 
   tags = {
     Name = "terraform"
