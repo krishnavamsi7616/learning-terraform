81e110bd57b5f47c21ba4af30b83686e52955590
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-07-28 13:53:31 -0700

    updating ./learning-terraform

--- 06-conditions/main.tf
+++ 06-conditions/main.tf
@@ -19,6 +19,6 @@ locals {
 data "aws_ami" "example" {
   executable_users = ["self"]
   most_recent      = true
-  name_regex       = "base-with-ansible"
+  name_regex       = "base-image-with-ansible-installed"
   owners           = ["self"]
 }
